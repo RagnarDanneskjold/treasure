3b89f5a1f878b68334781983c477eb8757051ecf
Ryan X. Charles <ryan@bitpay.com>
2014-03-10 21:58:06 -0400

    added basic structure of treasure

--- treasure.js
+++ treasure.js
@@ -1,8 +1,47 @@
-var Treasure = function(M, N) {
+var imports = require('soop').imports();
+var bitcore = imports.bitcore || require('bitcore');
+//var Cosign = imports.Cosign || require('cosign');
+//var Insight = imports.Insight || require('insight-bitcore-api');
+
+var Treasure = function(M, N, name) {
   var self = this;
   self.M = M;
   self.N = N;
+  self.name = name;
 };
 
-Treasure.prototype.addPublicKeys = function() {
+Treasure.prototype.addPubKeys = function(user, pubkeys, signature) {
 };
+
+Treasure.prototype.backup = function() {
+};
+
+Treasure.prototype.broadcastTX = function(tx) {
+};
+
+Treasure.prototype.getBalance = function() {
+};
+
+Treasure.prototype.getMembers = function() {
+};
+
+Treasure.prototype.getNextReceivingAddress = function() {
+};
+
+Treasure.prototype.hasAllPubKeys = function() {
+};
+
+Treasure.prototype.hasNextReceivingAddress = function() {
+  var self = this;
+  return self.hasAllPubKeys();
+};
+
+Treasure.prototype.newPTX = function(amountBTC, address) {
+};
+
+Treasure.prototype.validatePTX = function(ptx) {
+};
+
+Treasure.prototype.signPTX = function(ptx, privkey) {
+};
+
