6d2fb3d69616d85f225db12a31b4d74880d7e419
Ryan X. Charles <ryan@bitpay.com>
2014-03-12 19:08:26 -0400

    add redeemScript and address functions

--- treasure.js
+++ treasure.js
@@ -1,23 +1,18 @@
 var imports = require('soop').imports();
 var bitcore = imports.bitcore || require('bitcore');
 
-var Treasure = function(M, N) {
+var Treasure = function(network, M, N) {
   var self = this;
+  self.network = bitcore.networks[network];
   self.M = 3;
   self.N = 5;
-  self.SINs = []; //string
-  self.pubKeys = {}; //array of buffer pubKeys for each SIN
+  self.SINs = []; //array of SINs (strings)
+  self.pubKeys = {}; //array of pubKeys (buffers) for each SIN
 };
 
-Treasure.prototype.addPubKey = function(SIN, pubKeyHex) {
+Treasure.prototype.addPubKey = function(SIN, pubKey) {
   var self = this;
   if (self.SINs.indexOf(SIN) > -1) {
-    try {
-      var pubKey = new Buffer(pubKeyHex, 'hex');
-    }
-    catch (e) {
-      return false;
-    }
     self.pubKeys[SIN].push(pubKey);
     return true;
   }
@@ -26,10 +21,36 @@ Treasure.prototype.addPubKey = function(SIN, pubKeyHex) {
   }
 };
 
-Treasure.prototype.getSortedPubKeys = function(index) {
+Treasure.prototype.addPubKeyHex = function(SIN, pubKeyHex) {
   var self = this;
-  var pubKeys = [];
+  try {
+    var pubKey = new Buffer(pubKeyHex, 'hex');
+  }
+  catch (e) {
+    return false;
+  }
+  self.addPubKey(pubKey);
+  return true;
+};
 
+Treasure.prototype.getAddress = function(index) {
+  var self = this;
+  var redeemScript = self.getRedeemScript(index);
+  var hash = bitcore.util.sha256ripe160(redeemScript);
+  var addr = new bitcore.Address(hash, self.network.addressScript);
+  return addr;
+};
+
+Treasure.prototype.getAddressStr = function(index) {
+  var self = this;
+  var address = self.getAddress(index);
+  return address.as('base58');
+};
+
+Treasure.prototype.getPubKeys = function(index) {
+  var self = this;
+
+  var pubKeys = [];
   for (var i in self.SINs) {
     var SIN = self.SINs[i];
     var pubKey = self.pubKeys[SIN][index];
@@ -37,6 +58,20 @@ Treasure.prototype.getSortedPubKeys = function(index) {
     if (!pubKey)
       return false;
   }
+
+  return pubKeys;
+};
+
+Treasure.prototype.getRedeemScript = function(index) {
+  var self = this;
+  var pubKeys = self.getSortedPubKeys(index);
+  var script = bitcore.Script.createMultisig(self.N, pubKeys);
+  return script;
+};
+
+Treasure.prototype.getSortedPubKeys = function(index) {
+  var self = this;
+  var pubKeys = self.getPubKeys(index);
 
   //sort lexicographically, i.e. as strings, i.e. alphabetically
   return pubKeys.sort();
