a836760d7681f0582f312c2f27832aa30da5f661
Ryan X. Charles <ryan@bitpay.com>
2014-03-12 14:15:00 -0400

    remove superfluous sort function; change pubkeys to bufs

--- treasure.js
+++ treasure.js
@@ -1,19 +1,23 @@
 var imports = require('soop').imports();
 var bitcore = imports.bitcore || require('bitcore');
-//var Cosign = imports.Cosign || require('cosign');
-//var Insight = imports.Insight || require('insight-bitcore-api');
 
 var Treasure = function(M, N) {
   var self = this;
   self.M = 3;
   self.N = 5;
-  self.SINs = [];
-  self.pubKeys = {}; //array of hex pubKeys for each SIN
+  self.SINs = []; //string
+  self.pubKeys = {}; //array of buffer pubKeys for each SIN
 };
 
-Treasure.prototype.addPubKey = function(SIN, pubKey) {
+Treasure.prototype.addPubKey = function(SIN, pubKeyHex) {
   var self = this;
   if (self.SINs.indexOf(SIN) > -1) {
+    try {
+      var pubKey = new Buffer(pubKeyHex, 'hex');
+    }
+    catch (e) {
+      return false;
+    }
     self.pubKeys[SIN].push(pubKey);
     return true;
   }
@@ -22,32 +26,18 @@ Treasure.prototype.addPubKey = function(SIN, pubKey) {
   }
 };
 
-Treasure.prototype.getSortedPubKeys = function(i) {
+Treasure.prototype.getSortedPubKeys = function(index) {
   var self = this;
   var pubKeys = [];
+
   for (var i in self.SINs) {
     var SIN = self.SINs[i];
-    var pubKey = self.pubKeys[SIN];
+    var pubKey = self.pubKeys[SIN][index];
     pubKeys.push(pubKey);
     if (!pubKey)
       return false;
   }
-  return Treasure.sortPubKeys(pubKeys);
-};
-
-Treasure.sortPubKeys = function(pubKeys) {
-  var pubKeysBuf = [];
-
-  for (var i in pubKeys) {
-    try {
-      pubKeysBuf[i] = new Buffer(pubKeys[i], 'hex');
-    } catch (e) {
-      return false;
-    }
-  }
 
   //sort lexicographically, i.e. as strings, i.e. alphabetically
-  pubKeysBuf.sort();
-
-  return pubKeysBuf;
+  return pubKeys.sort();
 };
