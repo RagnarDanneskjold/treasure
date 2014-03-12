54b0b9edf2d495f3ae2b654810d9daff8266f553
Ryan X. Charles <ryan@bitpay.com>
2014-03-11 22:26:48 -0400

    first untested implementation of public key sorting
    When generating a p2sh address, the address is different if the public keys are
    in a different order. My proposed solution to this to sort the public keys by
    treating them as big endian numbers. I will advocate for this standard to be
    adopted in the bitcoin world for all p2sh address generation. If the community
    adopts a different standard, we will adopt whichever one is chosen by the
    community. But for now it's this.

--- treasure.js
+++ treasure.js
@@ -3,45 +3,79 @@ var bitcore = imports.bitcore || require('bitcore');
 //var Cosign = imports.Cosign || require('cosign');
 //var Insight = imports.Insight || require('insight-bitcore-api');
 
-var Treasure = function(M, N, name) {
+var Treasure = function(M, N) {
   var self = this;
-  self.M = M;
-  self.N = N;
-  self.name = name;
+  self.M = 5;
+  self.N = 3;
+  self.SINs = [];
+  self.pubKeys = {}; //array of hex pubKeys for each SIN
 };
 
-Treasure.prototype.addPubKeys = function(user, pubkeys, signature) {
-};
-
-Treasure.prototype.backup = function() {
-};
-
-Treasure.prototype.broadcastTX = function(tx) {
-};
-
-Treasure.prototype.getBalance = function() {
-};
-
-Treasure.prototype.getMembers = function() {
-};
-
-Treasure.prototype.getNextReceivingAddress = function() {
-};
-
-Treasure.prototype.hasAllPubKeys = function() {
-};
-
-Treasure.prototype.hasNextReceivingAddress = function() {
+Treasure.prototype.addPubKey = function(SIN, pubKey) {
   var self = this;
-  return self.hasAllPubKeys();
+  if (self.SINs.indexOf(SIN) > -1) {
+    self.pubKeys[SIN].push(pubKey);
+    return true;
+  }
+  else {
+    return false;
+  }
 };
 
-Treasure.prototype.newPTX = function(amountBTC, address) {
+Treasure.prototype.getSortedPubKeys = function(i) {
+  var self = this;
+  var pubKeys = [];
+  for (var i in self.SINs) {
+    var SIN = self.SINs[i];
+    var pubKey = self.pubKeys[SIN];
+    pubKeys.push(pubKey);
+    if (!pubKey)
+      return false;
+  }
+  return Treasure.sortPubKeys(pubKeys);
 };
 
-Treasure.prototype.validatePTX = function(ptx) {
-};
+Treasure.sortPubKeys = function(pubKeys) {
+  var pubKeysBuf = [];
 
-Treasure.prototype.signPTX = function(ptx, privkey) {
-};
+  for (var i in pubKeys) {
+    try {
+      pubKeysBuf[i] = new Buffer(pubKeys[i], 'hex');
+    } catch (e) {
+      return false;
+    }
+  }
 
+  pubKeysBuf.sort(function(k1, k2) {
+    //first, make them the same length
+    if (k1.length > k2.length) {
+      var k = new Buffer(k1.length);
+      k.fill(0);
+      var i = k1.length - k2.length;
+      k2.copy(k, k1.length - k2.length);
+      k2 = k;
+    }
+    else if (k2.length > k1.length) {
+      var k = new Buffer(k2.length);
+      k.fill(0);
+      var i = k2.length - k1.length;
+      k1.copy(k, k2.length - k1.length);
+      k1 = k;
+    }
+    
+    //sort as big endian numbers
+    for (var i in k1) {
+      var x = k1[i];
+      var y = k2[i];
+      if (x < y)
+        return -1;
+      if (x > y)
+        return 1;
+      else
+        continue;
+    }
+    return 0;
+  });
+
+  return pubKeysBuf;
+};
